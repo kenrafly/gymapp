18d8563828de217827d12c6b2ebaf94d78b895a3
Rafly Ardiansyah <rafly21ardiansyah@gmail.com>
2024-10-26 16:27:41 +0700

    fixed

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,7 +1,7 @@
 export const exerciseOptions = {
   method: "GET",
   headers: {
-    "x-rapidapi-key": "b9554be384msh17e06d4922d6e15p1637afjsn4867dcdb078e",
+    "x-rapidapi-key": import.meta.env.VITE_API_KEY,
     "x-rapidapi-host": "exercisedb.p.rapidapi.com",
   },
 };
@@ -9,7 +9,7 @@ export const exerciseOptions = {
 export const youtubeOptions = {
   method: "GET",
   headers: {
-    "x-rapidapi-key": "b9554be384msh17e06d4922d6e15p1637afjsn4867dcdb078e",
+    "x-rapidapi-key": import.meta.env.VITE_YT_KEY,
     "x-rapidapi-host": "youtube-search-and-download.p.rapidapi.com",
   },
 };
