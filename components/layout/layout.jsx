c2303521181f602afe6137bf0128cdfd39d3fef9
Rafly Ardiansyah <rafly21ardiansyah@gmail.com>
2024-10-26 16:31:29 +0700

    test

--- components/layout/layout.jsx
+++ components/layout/layout.jsx
@@ -1,7 +1,6 @@
 // src/components/Layout/Layout.jsx
 import React from "react";
 import { Outlet } from "react-router-dom";
-import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
 
 const Layout = () => {
